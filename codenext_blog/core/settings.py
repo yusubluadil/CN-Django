80ad7eb17f0f6d8222de92e7f6a085018a600eed
Adil <yusubluadil00@gmail.com>
2024-12-07 17:36:18 +0400

    updated

--- codenext_blog/core/settings.py
+++ codenext_blog/core/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-226jrix0qgxbb2w5l33a1xyd&b*pk3le#-akkb)bbobtm!uqi*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['turbo.az']
 
 
 # Application definition
